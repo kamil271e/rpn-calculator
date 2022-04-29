cee782eacede098a293f235a32d03b7390e8f995
kamil271e <kamil271e@gmail.com>
2022-04-29 16:02:43 +0200

    Different approach & stack view

--- app/src/main/java/com/example/rpn_calc/MainActivity.kt
+++ app/src/main/java/com/example/rpn_calc/MainActivity.kt
@@ -8,46 +8,75 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    private var canAddOp = false
-    private var canDecimal = true
+    private var canPushStack = true
+    private var canUseOperator = false
+    private var canPutComma = false
+    private var stack = ArrayDeque<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
-    fun numAction(view: View) {
+    fun numberAction(view: View) {
         if (view is Button){
-            if (view.text == "."){
-                if (canDecimal){
-                    workTextView.append(view.text)
-                }
-                canDecimal = false
-            }
-            else
-                workTextView.append(view.text)
-            canAddOp = true
+            calcTextView.append(view.text)
         }
     }
-    fun opAction(view: View) {
+    fun operationAction(view: View) {
+        val data = calcTextView.text.toString()
         if (view is Button) {
-            workTextView.append(view.text)
-            canAddOp = false
-            canDecimal = true
+            if (view.text == "."){
+                val index = -1
+                if (data.indexOf(".", index) == -1 && data.isNotEmpty())
+                    calcTextView.append(view.text)
+            }
+            else calcTextView.append(view.text)
         }
     }
     fun clearAction(view: View) {
-        workTextView.text = ""
-        resTextView.text = ""
+        calcTextView.text = ""
+        stack.clear()
     }
-    fun equalAction(view: View) {
-        val temp = solve().toString()
-        clearAction(view)
-        workTextView.text = temp
+    fun enterAction(view: View) {
+
+        val calcText = calcTextView.text.toString()
+        var op = -1
+        if (calcText.isEmpty()) return
+
+        try {
+            calcText.toInt()
+        }catch (e: NumberFormatException){
+            canPushStack = false
+        }
+
+        if (canPushStack) { // adding values to stack
+            stack.add(calcText)
+            calcTextView.text = ""
+            updateStackView()
+
+        }else{
+            when (calcText) {
+                "+" -> op = 1
+                "*" -> op = 2
+                "-" -> op = 3
+                "/" -> op = 4
+            }
+            //operation(stack.removeLast().toInt(), stack.removeLast().toInt(), op)?.let { stack.add(it.toString()) }
+            val a1 = stack.removeLast().toInt()
+            val a2 = stack.removeLast().toInt()
+            // Log.i("inside","$calcText $op $a1 $a2")
+            val result = operation(a1, a2, op).toString()
+            canPushStack = true
+            calcTextView.text = result
+        }
+        //Log.i("stack_values", stack.toString())
     }
 
     fun backSpaceAction(view: View) {
-        val len = workTextView.length()
+        val len = calcTextView.length()
         if(len > 0) {
-            workTextView.text = workTextView.text.subSequence(0, len-1)
+            calcTextView.text = calcTextView.text.subSequence(0, len-1)
         }
     }
 
@@ -58,73 +87,28 @@ class MainActivity : AppCompatActivity() {
             3 -> return b-a
             4 -> {
                 if(a!=0) {
-                    Log.i("division", "$a $b") // czyta dzielenie w odwr kolejnosci
+                    //Log.i("division", "$a $b") // division in reversed order
                     return b/a
                 }
             }
-            else -> print("Coś poszło nie tak...")
+            else -> Log.i("error","Something went wrong...")
         }
         return null
     }
 
-    private fun readEquation(): MutableList<String> { // text view represented as a list of num-s and op-s
-        /*var currDigit = ""
-        for (c in workTextView.text){
-            if (c.isDigit()){
-                currDigit += c
-            }
-            else{
-                data.add(currDigit)
-                currDigit = ""
-                data.add(c.toString())
+    private fun updateStackView(){
+        var temp = ""
+        if (stack.size < 4){
+            for (i in 1..(4 - stack.size)){
+                temp += "0\n"
             }
         }
-        if (currDigit != "") data.add(currDigit)*/
-        /*var currDigit = ""
-        for (c in workTextView.text){
-            if(c.isDigit() || c == '.'){
-                currDigit += c
-            }
-            else{
-                list.add(currDigit.toFloat())
-                currDigit = ""
-                list.add(c) // adding operator
-            }
+        val end = if (stack.size > 4) 4
+        else stack.size
+        for (i in end downTo 1){
+            temp += stack[stack.size-i] + "\n"
         }
-        if (currDigit != ""){
-            list.add(currDigit.toFloat())
-        }*/
-        return workTextView.text.split(" ") as MutableList<String>
-    }
-
-    private fun solve(): Int {
-        val stack = ArrayDeque<Int>()
-        val equation = readEquation()
-        var op = -1
-        // Log.i("stack", equation.toString())
-        if (equation.isEmpty()) return 0
-        for (x in equation){
-            var flag = false
-            for (c in x){
-                if (!c.isDigit()) flag = true
-            }
-            if (!flag){ // numbers
-                stack.add(x.toInt())
-            }
-            else{ // operator occurred
-                when (x) {
-                    "+" -> op = 1
-                    "*" -> op = 2
-                    "-" -> op = 3
-                    "/" -> op = 4
-                }
-                operation(stack.removeLast(), stack.removeLast(), op)?.let { stack.add(it) }
-            }
-        }
-        return stack.last()
-    }
-
-    fun spaceAction(view: View) {
-        workTextView.append(" ")
+        Log.i("stack_view", temp)
+        stackTextView.text = temp.subSequence(0, temp.length-1)
     }
 }
